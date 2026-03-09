ccaca4163c291ce3d2cd26e2c9172cff52f43ad5
bartoscsistvan <22c_gyapjasa@nyirszikszi.hu>
2026-03-09 09:19:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//Gyapjas Andras 12/C
+
+function go(){
+    let toConvert = document.getElementById("selectors").value;
+    let linker =`https://hexarate.paikama.co/api/rates/HUF/${toConvert}/latest`; 
+
+    fetch(linker)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+    calculator(data);
+});
+}
+
+let displayer = document.getElementById("ret");
+
+function calculator(data){
+    let p = document.getElementById("ret");
+    let money2Conv = document.getElementById("money").value;
+    p.innerHTML = (money2Conv*data.data.mid);
+}
+
